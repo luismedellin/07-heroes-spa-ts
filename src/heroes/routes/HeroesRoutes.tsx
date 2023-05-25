8d585a43314b6c44d73aa3a6c3de819dc2c5e2f5
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-24 21:22:50 -0500

    14: 192. Creando un segundo Router

---
+++ src/heroes/routes/HeroesRoutes.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { DcPage, HeroPage, MarvelPage, SearchPage } from "..";
+import { Navbar } from "../../ui";
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DcPage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="hero" element={<HeroPage />} />
+
+        {/* Search, Heroe by id */}
+
+        <Route path="/" element={<Navigate to="marvel" />} />
+      </Routes>
+    </>
+  );
+};
--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -1,51 +1,45 @@
-import { Link, NavLink } from 'react-router-dom';
-
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+  const setActive = (isActive: boolean) =>
+    `nav-item nav-link ${isActive ? "active" : ""}`;
 
-    const setActive = (isActive: boolean) => `nav-item nav-link ${isActive ? 'active': ''}`
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Asociaciones
+      </Link>
 
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Asociaciones
-            </Link>
+      <div className="navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink
+            className={({ isActive }) => setActive(isActive)}
+            to="/marvel"
+          >
+            Marvel
+          </NavLink>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
-                    <NavLink 
-                        className={({isActive}) => setActive(isActive) } 
-                        to="/marvel"
-                    >
-                        Marvel
-                    </NavLink>
+          <NavLink className={({ isActive }) => setActive(isActive)} to="/dc">
+            DC
+          </NavLink>
 
-                    <NavLink 
-                        className={({isActive}) => setActive(isActive) } 
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
-                </div>
-            </div>
+          <NavLink className={({ isActive }) => setActive(isActive)} to="/search">
+            Search
+          </NavLink>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end p-2">
-                <ul className="navbar-nav ml-auto">
-                    <span className="nav-item nav-link text-primary">
-                        Luis
-                    </span>
+          <NavLink className={({ isActive }) => setActive(isActive)} to="/hero">
+            Hero
+          </NavLink>
+        </div>
+      </div>
 
-                    <button
-                        className="nav-item nav-link btn"
-                    >
-                        Logout
-                    </button>
-                </ul>
-            </div>
-        </nav>
-    )
-}
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end p-2">
+        <ul className="navbar-nav ml-auto">
+          <span className="nav-item nav-link text-primary">Luis</span>
+
+          <button className="nav-item nav-link btn">Logout</button>
+        </ul>
+      </div>
+    </nav>
+  );
+};
