fc8a7777b27edc21785e2973b888aed9091856bb
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-24 20:26:45 -0500

    14: 188. Inicio de proyecto - HeroesApp

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 
+import { HeroesApp } from './HeroesApp';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HeroesApp />
   </React.StrictMode>,
 )
