197ca68a216e1911ebb803148b684eee3a5c7d83
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-24 23:29:25 -0500

    14: 202. SearchComponent

--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -1,6 +1,24 @@
-import { HeroCard } from "..";
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { useForm } from "../../hooks";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {q = ''} = queryString.parse(location.search);
+  
+
+  const {searchText, onInputChange} = useForm({
+    searchText: ''
+  });
+
+  const onSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if(searchText.trim().length <= 1) return;
+
+    navigate(`?q=${searchText}`)
+  }
+
   return (
     <>
       <h1>Search</h1>
@@ -10,13 +28,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
             />
 
             <button className="btn btn-outline-primary mt-1">Search</button>
@@ -32,7 +52,7 @@ export const SearchPage = () => {
           </div>
 
           <div className="alert alert-danger">
-            No hero with <b>ABC</b>
+            No hero with <b>{q}</b>
           </div>
 
           {/* <HeroCard  /> */}
