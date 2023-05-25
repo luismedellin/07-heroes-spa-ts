076422c42af8c3bb828f6e0475fd064ee3d246d5
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-25 15:01:33 -0500

    15: 214. Rutas privadas

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,13 +2,20 @@ import { Route, Routes } from "react-router-dom";
 
 import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        } />
+        {/* <Route path="/*" element={<HeroesRoutes />} /> */}
       </Routes>
     </>
   );
