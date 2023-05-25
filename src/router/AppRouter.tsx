27bf7e6818723d563eeeb973fd5dbb17b9fed649
Luis Velandia <luiseduardo1218@gmail.com>
2023-05-24 20:59:43 -0500

    14: 191. Colocar clase de la ruta activa
    - Logout
    - stilos al logout

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,19 +1,22 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { LoginPage } from "../auth/pages/LoginPage"
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import { DcPage, MarvelPage } from "../heroes";
+import { LoginPage } from "../auth";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="marvel" element={<MarvelPage />} />
-            <Route path="dc" element={<DcPage />} />
+      <Navbar />
 
-            <Route path="login" element={<LoginPage />} />
+      <Routes>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DcPage />} />
 
-            <Route path="/" element={<Navigate to="marvel" />} />
-        </Routes>
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="/" element={<Navigate to="marvel" />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
---
+++ src/ui/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import { Link, NavLink } from 'react-router-dom';
+
+
+export const Navbar = () => {
+
+    const setActive = (isActive: boolean) => `nav-item nav-link ${isActive ? 'active': ''}`
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                Asociaciones
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+                    <NavLink 
+                        className={({isActive}) => setActive(isActive) } 
+                        to="/marvel"
+                    >
+                        Marvel
+                    </NavLink>
+
+                    <NavLink 
+                        className={({isActive}) => setActive(isActive) } 
+                        to="/dc"
+                    >
+                        DC
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end p-2">
+                <ul className="navbar-nav ml-auto">
+                    <span className="nav-item nav-link text-primary">
+                        Luis
+                    </span>
+
+                    <button
+                        className="nav-item nav-link btn"
+                    >
+                        Logout
+                    </button>
+                </ul>
+            </div>
+        </nav>
+    )
+}
